1af212c1ccbc0ea074ab93dc437daccfb09c2dfd
FidelisMarcelino <fidelis.binus@gmail.com>
2025-05-20 20:38:27 +0700

    Revisi 1 Fidel

--- src/demo/java/hu/webarticum/treeprinter/demo/ExamplesMain.java
+++ src/demo/java/hu/webarticum/treeprinter/demo/ExamplesMain.java
@@ -1,64 +1,94 @@
 package hu.webarticum.treeprinter.demo;
 
-import hu.webarticum.treeprinter.Insets;
-import hu.webarticum.treeprinter.PlaceholderNode;
-import hu.webarticum.treeprinter.SimpleTreeNode;
-import hu.webarticum.treeprinter.TreeNode;
-import hu.webarticum.treeprinter.decorator.BorderTreeNodeDecorator;
-import hu.webarticum.treeprinter.decorator.PadTreeNodeDecorator;
-import hu.webarticum.treeprinter.decorator.ShadowTreeNodeDecorator;
+import hu.webarticum.treeprinter.*;
+import hu.webarticum.treeprinter.decorator.*;
 import hu.webarticum.treeprinter.printer.boxing.BoxingTreePrinter;
 import hu.webarticum.treeprinter.printer.listing.ListingTreePrinter;
 import hu.webarticum.treeprinter.printer.traditional.TraditionalTreePrinter;
 
+/*
+ * Smells dalam kode
+ * 1. Long Method			: solusinya extract method
+ * 2. Primitive Obsession	: solusinya replace data value dengan object
+ * 3. Feature Envy			: solusinya extract method
+ */
+
 public class ExamplesMain {
-    
+
+    private static final int SEPARATOR_WIDTH = 75;
+
     public static void main(String[] args) {
         TreeNode rootNode = buildTree();
-        
-        new ListingTreePrinter().print(rootNode);
 
+        printAllVariants(rootNode);
+    }
+
+    private static void printAllVariants(TreeNode rootNode) {
+        printDefaultListing(rootNode);
         printSeparator();
 
-        ListingTreePrinter.builder().ascii().liningSpace("...").build().print(rootNode);
-
+        printAsciiListing(rootNode);
         printSeparator();
 
-        ListingTreePrinter.builder().displayRoot(false).align(true).build().print(
-                PadTreeNodeDecorator.builder()
-                        .bottomPad(1)
-                        .buildFor(rootNode)
-                );
-
+        printAlignedListingWithPadding(rootNode);
         printSeparator();
 
+        printBorderedListingWithPadding(rootNode);
+        printSeparator();
+
+        printTraditionalWithShadow(rootNode);
+        printSeparator();
+
+        printBoxing(rootNode);
+    }
+
+    private static void printDefaultListing(TreeNode node) {
+        new ListingTreePrinter().print(node);
+    }
+
+    private static void printAsciiListing(TreeNode node) {
+        ListingTreePrinter.builder()
+                .ascii()
+                .liningSpace("...")
+                .build()
+                .print(node);
+    }
+
+    private static void printAlignedListingWithPadding(TreeNode node) {
+        ListingTreePrinter.builder()
+                .displayRoot(false)
+                .align(true)
+                .build()
+                .print(PadTreeNodeDecorator.builder().bottomPad(1).buildFor(node));
+    }
+
+    private static void printBorderedListingWithPadding(TreeNode node) {
         new ListingTreePrinter().print(
                 new BorderTreeNodeDecorator(
                         PadTreeNodeDecorator.builder()
                                 .verticalPad(1)
                                 .horizontalPad(2)
-                                .buildFor(rootNode)
-                        ));
+                                .buildFor(node)));
+    }
 
-        printSeparator();
-
+    private static void printTraditionalWithShadow(TreeNode node) {
         new TraditionalTreePrinter().print(
                 new ShadowTreeNodeDecorator(
                         BorderTreeNodeDecorator.builder().wideUnicode().buildFor(
-                                new PadTreeNodeDecorator(rootNode, new Insets(0, 1))
-                                )));
-
-        printSeparator();
-
-        new BoxingTreePrinter().print(rootNode);
-
+                                new PadTreeNodeDecorator(node, new Insets(0, 1)))));
     }
-    
+
+    private static void printBoxing(TreeNode node) {
+        new BoxingTreePrinter().print(node);
+    }
+
     private static TreeNode buildTree() {
         TestNode rootNode = new TestNode("root");
+
         TestNode subNode1 = new TestNode("SUB asdf\nSSS fdsa\nxxx yyy");
         TestNode subNode2 = new TestNode("lorem ipsum");
         TestNode subNode3 = new TestNode("ggggg");
+
         TestNode subSubNode11 = new TestNode("AAA");
         TestNode subSubNode12 = new TestNode("BBB");
         TestNode subSubNode21 = new TestNode("CCC");
@@ -66,14 +96,16 @@ public class ExamplesMain {
         TestNode subSubNode23 = new TestNode("EEE");
         TestNode subSubNode24 = new TestNode("FFF");
         TestNode subSubNode31 = new TestNode("GGG");
+
         TestNode subSubSubNode231 = new TestNode("(eee)");
         TestNode subSubSubNode232 = new TestNode("(eee2)");
         TestNode subSubSubNode311 = new TestNode("(ggg)");
 
         rootNode.addChild(subNode1);
-        rootNode.addChild(new PlaceholderNode()); // this should not be displayed
+        rootNode.addChild(new PlaceholderNode());
         rootNode.addChild(subNode2);
         rootNode.addChild(subNode3);
+
         subNode1.addChild(subSubNode11);
         subNode1.addChild(subSubNode12);
         subNode2.addChild(subSubNode21);
@@ -81,22 +113,20 @@ public class ExamplesMain {
         subNode2.addChild(subSubNode23);
         subNode2.addChild(subSubNode24);
         subNode3.addChild(subSubNode31);
+
         subSubNode23.addChild(subSubSubNode231);
         subSubNode23.addChild(subSubSubNode232);
         subSubNode31.addChild(subSubSubNode311);
-        
+
         return rootNode;
     }
-    
+
     private static void printSeparator() {
         System.out.println();
-        for (int i = 0; i < 75; i++) {
-            System.out.print('=');
-        }
-        System.out.println();
+        System.out.println("=".repeat(SEPARATOR_WIDTH));
         System.out.println();
     }
-    
+
     private static class TestNode extends SimpleTreeNode {
 
         TestNode(String content) {
@@ -106,9 +136,7 @@ public class ExamplesMain {
         @Override
         public boolean isDecorable() {
             String plainContent = content().plain();
-            return (plainContent.isEmpty() || plainContent.charAt(0) != '(');
+            return plainContent.isEmpty() || plainContent.charAt(0) != '(';
         }
-        
     }
-    
 }
