103e5fd163791d4304c82caae058037f5ea3bbce
FidelisMarcelino <fidelis.binus@gmail.com>
2025-05-26 13:34:15 +0700

    Fidel - Long Method & Primitive Obsession

--- src/main/java/hu/webarticum/treeprinter/text/AnsiLineMerger.java
+++ src/main/java/hu/webarticum/treeprinter/text/AnsiLineMerger.java
@@ -7,89 +7,108 @@ public class AnsiLineMerger implements LineMerger {
     
     private static final char ESCAPE_CHAR = '\u001B';
 
-    private static final Pattern ANSI_ESCAPE_START_PATTERN = Pattern.compile("^\\e(?:\\[[0-9;]*m)?");
+    private static final Pattern ANSI_ESCAPE_PATTERN = Pattern.compile("^\\e(?:\\[[0-9;]*m)?");
     
+    private static final String ANSI_RESET = TextUtil.ansiReset();
 
     @Override
     public String merge(String existingLine, int fromPosition, String replacement) {
         StringBuilder resultBuilder = new StringBuilder();
-        
-        int replacementLength = replacement.length();
-        StringBuilder replacementEscapeStack = new StringBuilder();
-        int replacementAfterLocation = replacementLength + 1;
-        int replacementDiff = locate(replacement, 0, 0, replacementAfterLocation, replacementEscapeStack);
-        
-        int replacementWidth = replacementAfterLocation + replacementDiff;
-        int replacementVisibleEnd = fromPosition + replacementWidth;
 
-        StringBuilder leadingEscapeStack = new StringBuilder();
-        int leadingLength = -fromPosition;
-        if (fromPosition > 0 && !existingLine.isEmpty()) {
-            leadingLength = locate(existingLine, 0, 0, fromPosition, leadingEscapeStack);
-            String leading = leadingLength > 0 ? existingLine.substring(0, leadingLength) : existingLine;
-            resultBuilder.append(leading);
-            if (leadingEscapeStack.length() > 0) {
-                resultBuilder.append(TextUtil.ansiReset());
-            }
-        }
-        
-        if (leadingLength < 0) {
-            TextUtil.repeat(resultBuilder, ' ', -leadingLength);
-        }
-        
-        resultBuilder.append(replacement);
-        if (replacementEscapeStack.length() > 0) {
-            resultBuilder.append(TextUtil.ansiReset());
-        }
+        int replacementWidth = calculateVisibleWidth(replacement);
+        int replacementEnd = fromPosition + replacementWidth;
+
+        int leadingLength = handleLeading(existingLine, fromPosition, resultBuilder);
+        appendReplacement(replacement, resultBuilder);
 
         if (leadingLength >= 0 && !existingLine.isEmpty()) {
-            StringBuilder innerEscapeStack = new StringBuilder();
-            int innerRight = locate(existingLine, leadingLength, fromPosition, replacementVisibleEnd, innerEscapeStack);
-            if (innerRight >= 0) {
-                resultBuilder.append(leadingEscapeStack);
-                resultBuilder.append(innerEscapeStack);
-                resultBuilder.append(existingLine.substring(innerRight));
-            }
+            appendTrailing(existingLine, leadingLength, fromPosition, replacementEnd, resultBuilder);
         }
+
         return resultBuilder.toString();
     }
+
+    private int handleLeading(String existingLine, int fromPosition, StringBuilder resultBuilder) {
+        if (fromPosition <= 0 || existingLine.isEmpty()) {
+            TextUtil.repeat(resultBuilder, ' ', -fromPosition);
+            return -fromPosition;
+        }
+
+        StringBuilder leadingEscapeStack = new StringBuilder();
+        int leadingLength = locateVisible(existingLine, 0, 0, fromPosition, leadingEscapeStack);
+
+        String leadingText = leadingLength > 0 ? existingLine.substring(0, leadingLength) : existingLine;
+        resultBuilder.append(leadingText);
+
+        if (leadingEscapeStack.length() > 0) {
+            resultBuilder.append(ANSI_RESET);
+        }
+
+        return leadingLength;
+    }
+
+    private void appendReplacement(String replacement, StringBuilder resultBuilder) {
+        StringBuilder escapeStack = new StringBuilder();
+        locateVisible(replacement, 0, 0, replacement.length() + 1, escapeStack);
+
+        resultBuilder.append(replacement);
+        if (escapeStack.length() > 0) {
+            resultBuilder.append(ANSI_RESET);
+        }
+    }
+
+    private void appendTrailing(String existingLine, int leadingLength, int fromPosition, int visibleEnd, StringBuilder resultBuilder) {
+        StringBuilder leadingEscapeStack = new StringBuilder();
+        locateVisible(existingLine, 0, 0, fromPosition, leadingEscapeStack);
+
+        StringBuilder trailingEscapeStack = new StringBuilder();
+        int trailingIndex = locateVisible(existingLine, leadingLength, fromPosition, visibleEnd, trailingEscapeStack);
+
+        if (trailingIndex >= 0) {
+            resultBuilder.append(leadingEscapeStack);
+            resultBuilder.append(trailingEscapeStack);
+            resultBuilder.append(existingLine.substring(trailingIndex));
+        }
+    }
+
+    private int calculateVisibleWidth(String line) {
+        StringBuilder stack = new StringBuilder();
+        int targetVisible = line.length() + 1;
+        int result = locateVisible(line, 0, 0, targetVisible, stack);
+        return targetVisible + result;
+    }
     
-    private int locate(String line, int startLocation, int startVisibleLocation, int targetVisibleLocation, StringBuilder escapeStack) {
+    private int locateVisible(String line, int startIdx, int visibleStart, int visibleTarget, StringBuilder escapeStack) {
         int length = line.length();
-        int location = startLocation;
-        int visibleLocation = startVisibleLocation;
-        while (location < length && visibleLocation < targetVisibleLocation) {
-            char c = line.charAt(location);
+        int idx = startIdx;
+        int visibleIdx = visibleStart;
+
+        while (idx < length && visibleIdx < visibleTarget) {
+            char c = line.charAt(idx);
             if (c == ESCAPE_CHAR) {
-                String ansiEscape = getAnsiEscape(line, location);
-                if (ansiEscape.equals(TextUtil.ansiReset())) {
+                String ansi = extractAnsiEscape(line, idx);
+                if (ANSI_RESET.equals(ansi)) {
                     escapeStack.setLength(0);
                 } else {
-                    escapeStack.append(ansiEscape);
+                    escapeStack.append(ansi);
                 }
-                location += ansiEscape.length();
+                idx += ansi.length();
             } else {
-                location++;
-                visibleLocation++;
+                idx++;
+                visibleIdx++;
             }
         }
-        
-        if (location < length && line.substring(location).equals(TextUtil.ansiReset())) {
+
+        if (idx < length && line.substring(idx).equals(ANSI_RESET)) {
             escapeStack.setLength(0);
             return length;
-        } else if (visibleLocation < targetVisibleLocation) {
-            return visibleLocation - targetVisibleLocation;
-        } else {
-            return location;
         }
+
+        return (visibleIdx < visibleTarget) ? visibleIdx - visibleTarget : idx;
     }
-    
-    private String getAnsiEscape(String line, int position) {
-        Matcher matcher = ANSI_ESCAPE_START_PATTERN.matcher(line.substring(position));
-        if (!matcher.find()) {
-            return ESCAPE_CHAR + "";
-        }
-        return matcher.group();
+
+    private String extractAnsiEscape(String line, int position) {
+        Matcher matcher = ANSI_ESCAPE_PATTERN.matcher(line.substring(position));
+        return matcher.find() ? matcher.group() : String.valueOf(ESCAPE_CHAR);
     }
-    
 }
